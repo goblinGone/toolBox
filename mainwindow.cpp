7ed027265cc57eb536568f8c0b2835d440f79444
goblinGone <jamsaware@gmail.com>
2023-06-24 21:01:17 +0100

    changes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,9 +13,24 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
 void MainWindow::on_pushButton_clicked()
 {
     ui->textBrowser->setText("Hello, world");
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->textBrowser->setText("hello from the other button");
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    ui->textBrowser->setText("another button push");
+}
+
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    ui->textBrowser->setText("last button push");
 }
 
